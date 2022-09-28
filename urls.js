5073c6332491ee4f88c470a2cf4d852fa6ddbcf3
Réminho <remi.corroy@gmail.com>
2022-09-28 17:32:14 +0200

    last one

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = 'vercel-backend-42sbov3yy-reminho78.vercel.app';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'vercel-frontend-rkui-6yyttma7s-reminho78.vercel.app';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
